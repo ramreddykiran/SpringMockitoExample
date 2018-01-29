74e11d74f9df9259a59db9cde610543dd05afec0
Kiran <ramreddykiran@gmail.com>
2018-01-29 21:21:27 +0530

    added user domain object and dummy controller and service classes

--- src/main/java/com/nisum/controller/UserController.java
+++ src/main/java/com/nisum/controller/UserController.java
@@ -1,6 +1,15 @@
 package com.nisum.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+import com.nisum.service.UserService;
+
+@Controller
 public class UserController {
-
+	
+	@Autowired
+	private UserService userService;
+	
+	
 }
--- src/test/java/com/nisum/controller/UserControllerTest.java
+++ src/test/java/com/nisum/controller/UserControllerTest.java
@@ -1,7 +1,21 @@
 package com.nisum.controller;
 
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.nisum.service.UserService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 	
+	@InjectMocks
 	private UserController userController;
+	
+	@Mock
+	private UserService userService;
+	
+	
 
 }
